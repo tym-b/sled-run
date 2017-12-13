3bf3e53401c5981f490f5c66ec0c88c6bd89e1db
Michał Kubiak <mkubiak@apptension.com>
2017-12-13 13:19:19 +0100

    Add resetting of reducer timeout on coin collide

--- src/components/game/engine/physics/objects/player.js
+++ src/components/game/engine/physics/objects/player.js
@@ -36,6 +36,7 @@ export default function createPlayer() {
     player.userData.objectsToRemove.push(object);
     player.userData.speed = player.userData.speedBooster;
 
+    clearTimeout(reducerTimeoutId);
     clearTimeout(boosterTimeoutId);
     boosterTimeoutId = resetSpeedBooster();
   };
