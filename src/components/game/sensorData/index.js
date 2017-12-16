b82bd1aec4a9db55fac27a9aa0307b5832be8d6c
Martin Kmieciak <mkmieciak@apptension.com>
2017-12-16 23:47:01 +0100

    Sensor data refactor

--- src/components/game/sensorData/index.js
+++ src/components/game/sensorData/index.js
@@ -10,40 +10,42 @@ export default class SensorData {
   socket = socketio(`${window.location.hostname}:8181`);
 
   constructor() {
-    this.socket.on('connect', () => {
-      this.socket.emit('gameConnected');
-    });
+    this.socket.on('connect', this.handleConnect);
+    this.socket.on('deviceMoveChanged', this.handleDeviceMoveChanged);
 
-    this.socket.on('deviceMoveChanged', ({ type, position }) => {
-      this.sensors[type] = position;
-    });
-
-    window.addEventListener('keydown', ({ key }) => {
-      switch (key) {
-        case 'ArrowLeft':
-          this.sensors[RED_PLAYER] = 5;
-          break;
-        case 'ArrowRight':
-          this.sensors[RED_PLAYER] = -5;
-          break;
-        case 'a':
-          this.sensors[GREEN_PLAYER] = 5;
-          break;
-        case 'd':
-          this.sensors[GREEN_PLAYER] = -5;
-          break;
-        default:
-      }
-    });
-
-    window.addEventListener('keyup', ({ key }) => {
-      if (key === 'ArrowLeft' || key === 'ArrowRight') {
-        this.sensors[RED_PLAYER] = 0;
-      } else if (key === 'a' || key === 'd') {
-        this.sensors[GREEN_PLAYER] = 0;
-      }
-    });
+    window.addEventListener('keydown', this.handleKeyDown);
+    window.addEventListener('keyup', this.handleKeyUp);
   }
+
+  handleKeyDown = ({ key }) => {
+    switch (key) {
+      case 'ArrowLeft':
+        this.sensors[RED_PLAYER] = 5;
+        break;
+      case 'ArrowRight':
+        this.sensors[RED_PLAYER] = -5;
+        break;
+      case 'a':
+        this.sensors[GREEN_PLAYER] = 5;
+        break;
+      case 'd':
+        this.sensors[GREEN_PLAYER] = -5;
+        break;
+      default:
+    }
+  };
+
+  handleKeyUp = ({ key }) => {
+    if (key === 'ArrowLeft' || key === 'ArrowRight') {
+      this.sensors[RED_PLAYER] = 0;
+    } else if (key === 'a' || key === 'd') {
+      this.sensors[GREEN_PLAYER] = 0;
+    }
+  };
+
+  handleConnect = () => this.socket.emit('gameConnected');
+
+  handleDeviceMoveChanged = ({ type, position }) => (this.sensors[type] = position);
 
   sendPlayerCollideEvent = (type) => this.socket.emit('playerCollided', { type });
 
