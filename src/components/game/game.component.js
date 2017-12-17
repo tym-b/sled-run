40d7632c18e4621cfde71a8d509d96f93f2d7f3f
Martin Kmieciak <mkmieciak@apptension.com>
2017-12-17 01:30:00 +0100

    Move sensorDate to this from componentDidMount

--- src/components/game/game.component.js
+++ src/components/game/game.component.js
@@ -13,8 +13,6 @@ export default class Game extends PureComponent {
   }
 
   componentDidMount() {
-    this.sensorData = new SensorData({ onCollide: this.handlePlayerCollided });
-
     this.socket.on('connect', this.handleConnect);
     this.socket.on('playerConnected', this.handlePlayerConnected);
     this.socket.on('playerDisconnected', this.handlePlayerDisconnected);
@@ -29,6 +27,7 @@ export default class Game extends PureComponent {
   }
 
   socket = socketio(`${window.location.hostname}:8181`);
+  sensorData = new SensorData({ onCollide: this.handlePlayerCollided });
 
   handlePlayerCollided = (type) => this.socket.emit('playerCollided', { type });
   handleConnect = () => this.socket.emit('gameConnected');
